c1dc6af4e22cf2f767344d8e399170146ccfd9eb
Rushi <rushikeshs855@gmail.com>
2025-09-02 22:24:46 +0530

    minor changes in compose file

--- services/order-service/internal/events/topics.go
+++ services/order-service/internal/events/topics.go
@@ -4,3 +4,6 @@ const(
 	NOTIFICATION_TOPIC = "notification-events"
 	ORDER_LOGS_TOPIC = "logs.order-service"
 )
+// docker exec -it kafka bash
+// kafka-topics --create --topic notification-events --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+// kafka-topics --create --topic logs.order-service --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
